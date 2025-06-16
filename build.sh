5646c321befb0c25e3594464c7a8205699e45ac1
Darth Livesey <szajcev504@gmail.com>
2025-06-16 07:25:52 +0300

    fourth step v1.0.9

--- build.sh
+++ build.sh
@@ -6,21 +6,16 @@ export PATH="/opt/render/project/python/Python-3.10.18/bin:$PATH"
 
 # Создать виртуальное окружение
 python -m venv .venv
-
-# Активировать окружение
 source .venv/bin/activate
 
-# Установить зависимости с помощью pip
+# Установить зависимости
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Установить пути
-export PYTHONPATH="${PYTHONPATH}:/opt/render/project/src"
-
-# Применить миграции (используя Python из виртуального окружения)
-.venv/bin/python manage.py migrate
+# Применить миграции (с правильным путем к manage.py)
+python src/manage.py migrate
 
 # Собрать статику
-.venv/bin/python manage.py collectstatic --noinput --clear
+python src/manage.py collectstatic --noinput --clear
 
 echo "### Build completed successfully ###"

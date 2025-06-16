6d56ebcf45e825d796c834e97a30233f3f119e04
Darth Livesey <szajcev504@gmail.com>
2025-06-16 06:58:43 +0300

    fourth step v1.0.5

--- build.sh
+++ build.sh
@@ -4,15 +4,14 @@ set -e
 # Явно указать Python 3.10
 export PATH="/opt/render/project/python/Python-3.10.18/bin:$PATH"
 
-# Установить uv локально
-python -m pip install --user uv==0.7.13
-export PATH="$HOME/.local/bin:$PATH"
-
 # Создать виртуальное окружение
 python -m venv .venv
 source .venv/bin/activate
 
-# Установить зависимости
+# Установить uv глобально (без --user)
+python -m pip install uv==0.7.13
+
+# Установить зависимости проекта
 uv pip install --python=python .
 
 # Применить миграции

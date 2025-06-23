ca20db134818f992630bd0c5e461503fee3a7553
Darth Livesey <szajcev504@gmail.com>
2025-06-24 02:39:00 +0300

    seventh step v1.0.5

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -e
 
-# Определяем путь к интерпретатору Python
-PYTHON_BIN="/opt/render/project/python/Python-$PYTHON_VERSION/bin/python"
-
 # Используем UV для установки зависимостей из requirements.txt
 echo "### Installing dependencies with UV ###"
 uv pip install -r requirements.txt
 
+# Добавляем путь к Python в PYTHONPATH
+export PYTHONPATH="${PYTHONPATH}:/opt/render/project/python/Python-${PYTHON_VERSION}/lib/python3.10/site-packages"
+
 # Компиляция переводов
 echo "### Compiling translations ###"
-$PYTHON_BIN src/manage.py compilemessages
+python src/manage.py compilemessages
 
 # Применение миграций
 echo "### Applying migrations ###"
-$PYTHON_BIN src/manage.py migrate
+python src/manage.py migrate
 
 # Сбор статики
 echo "### Collecting static files ###"
-$PYTHON_BIN src/manage.py collectstatic --noinput --clear
+python src/manage.py collectstatic --noinput --clear
 
 echo "### Build completed successfully ###"

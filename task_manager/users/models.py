c0df650db883043bdf5e55e0716dab40c5a02036
Darth Livesey <szajcev504@gmail.com>
2025-07-09 04:09:12 +0300

    Reorganizing the project structure v1.0.7

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-
 class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name=_("Дата создания"))
